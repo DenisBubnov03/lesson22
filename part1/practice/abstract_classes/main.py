0f7de633d144993a49aec568ad3c55cf1e14586b
denisbubnov03 <denisbu2000@gmail.com>
2022-09-05 18:21:32 +0300

    part 1

--- part1/practice/abstract_classes/main.py
+++ part1/practice/abstract_classes/main.py
@@ -39,8 +39,77 @@
 
 # код должен выполняться не выбрасывая исключений
 
-# TODO напишите Ваш код здесь
+from abc import ABC, abstractmethod
 
+
+class Transport(ABC):
+
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+    @abstractmethod
+    def stop_engine(self):
+        pass
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class Boat(Transport):
+    def start_engine(self):
+        print('Катер с трудом завелся ')
+
+    def stop_engine(self):
+        print('Двигатель катера заглох с концами ')
+
+    def move(self):
+        print('Катер быстро набирает скорость')
+
+    def stop(self):
+        print('Катер остановился')
+
+
+class Car(Transport):
+    def start_engine(self):
+        print('Машина бодро завелась')
+
+    def stop_engine(self):
+        print('Машина остановилась и заглушила двигатель ')
+
+    def move(self):
+        print('Машина едет по навигатору ')
+
+    def stop(self):
+        print('Машина остановилась')
+
+
+class Electroscooter(Transport):
+    def start_engine(self):
+        print('Издал звук запускающегося двигателя через динамик')
+
+    def stop_engine(self):
+        print('Издал звук заглушающегося двигателя ')
+
+    def move(self):
+        print('Прохожие в ужасе разбегаются от очередного камикадзе')
+
+    def stop(self):
+        print('Торможение об стену прошло успешно')
+
+
+class Person:
+
+    def use_transport(self, transport: Transport):
+        transport.start_engine()
+        transport.move()
+        transport.stop()
+        transport.stop_engine()
 
 # Отрезок кода для самопроверки.
 # Запустите его, после того как выполните задание
